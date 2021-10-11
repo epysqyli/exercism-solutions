2ef368f33215ba115147f82b19b7cb7d254583f8
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-11 16:30:31 +0200

    Composer PHPUnit install, remove comment

---
+++ php/run-length-encoding/RunLengthEncoding.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+function encode(string $input): string
+{
+    throw new \BadFunctionCallException("Implement the encode function");
+}
+
+function decode(string $input): string
+{
+    throw new \BadFunctionCallException("Implement the decode function");
+}
