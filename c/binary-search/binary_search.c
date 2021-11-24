02edc24b904becd3f54227c6350fc04edd12f740
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-24 14:34:37 +0100

    Add incomplete recursion

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -1,9 +1,45 @@
 #include "binary_search.h"
 
-int *binary_search(__attribute__((unused)) int value, const int *arr, size_t length)
+int counter = 0;
+
+int *binary_search(int value, const int *arr, size_t length)
 {
-  if (length == 1)
-    return (int *)&arr[0];
-  else
-    return 0;
+  int *address = NULL;
+
+  // even and odd cases?
+  size_t half = length / 2;
+
+  if (value == arr[half])
+  {
+    address = (int *)&arr[half];
+  }
+  else if (value == arr[0] && length == 1)
+  {
+    address = (int *)&arr[0];
+  }
+  else if (value < arr[half])
+  {
+    const int *left[half];
+    for (size_t i = 0; i < half; i++)
+    {
+      left[i] = &arr[i];
+    }
+
+    binary_search(value, *left, half);
+  }
+  else if (value > arr[half])
+  {
+    const int *right[half];
+    for (size_t i = 0; i < half; i++)
+    {
+      right[i] = &arr[half + i + 1];
+    }
+
+    binary_search(value, *right, half);
+  }
+
+  printf("Returned: %p - counter: %d\n", (void *)&arr[half], counter);
+  counter++;
+
+  return address;
 }
