83e10ed8d2f654d275a4eb1278b88f52a9cbb1cc
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-25 11:47:34 +0100

    Remove bool func

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -1,33 +1,20 @@
 #include "binary_search.h"
 
-bool in_array(int value, const int *arr, size_t length)
-{
-  bool res = false;
-  for (size_t i = 0; i < length; i++)
-  {
-    res = (arr[i] == value) ? true : false;
-    if (res)
-      break;
-  }
-
-  return res;
-}
-
 int *address = NULL;
 
 int *binary_search(int value, const int *arr, size_t length)
 {
-  if (!in_array(value, arr, length))
-    return NULL;
-
   size_t half = length / 2;
 
-  if (value == arr[half])
+  if (half == 0 && value != arr[half])
+  {
+    address = NULL;
+  }
+  else if (value == arr[half])
   {
     address = (int *)&arr[half];
   }
-
-  if (value < arr[half])
+  else if (value < arr[half])
   {
     const int *left[half];
     for (size_t i = 0; i < half; i++)
@@ -37,8 +24,7 @@ int *binary_search(int value, const int *arr, size_t length)
 
     binary_search(value, *left, half);
   }
-
-  if (value > arr[half])
+  else if (value > arr[half])
   {
     const int *right[half];
     for (size_t i = 0; i < half; i++)
