e97108ae012bd14903a6c76e28b25f51ea84c06a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-13 17:46:23 +0100

    Implement recursive find_node and rethink insert_item

--- c/binary-search-tree/binary_search_tree.c
+++ c/binary-search-tree/binary_search_tree.c
@@ -7,55 +7,58 @@
 
 node_t *build_tree(int *tree_data, size_t tree_data_len)
 {
+  // initialize head node
   node_t *head = malloc(sizeof(node_t));
 
   head->data = tree_data[0];
   head->left = NULL;
   head->right = NULL;
 
-  // build the whole tree
+  // build the tree
   for (size_t i = 1; i < tree_data_len; i++)
   {
-    node_t *current = find_node(head, tree_data[i]);
-    insert_item(current, tree_data[i]);
+    node_t *node = find_node(head, tree_data[i]);
+    insert_item(node, tree_data[i]);
   }
 
   return head;
 }
 
 // find the node whose left or right branch will be assigned the new item
-node_t *find_node(node_t *tree, int item)
+node_t *find_node(node_t *node, int item)
 {
-  if (tree->left == NULL && tree->right == NULL)
+  // case where item initially falls on the left
+  if (item <= node->data)
   {
-    printf("%d", item);
-    return tree;
+    if (node->left == NULL)
+    {
+      node_t *left = malloc(sizeof(node_t));
+      node->left = left;
+      return node->left;
+    }
+
+    find_node(node->left, item);
   }
 
-  return tree;
+  // case where item initially falls on the right
+  if (item > node->data)
+  {
+    if (node->right == NULL)
+    {
+      node_t *right = malloc(sizeof(node_t));
+      node->right = right;
+      return node->right;
+    }
+
+    find_node(node->right, item);
+  }
+
+  return node;
 }
 
 void insert_item(node_t *node, int item)
 {
-  if (item <= node->data)
-  {
-    node_t *new_node = malloc(sizeof(node_t));
-    new_node->data = item;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    node->left = new_node;
-  }
-
-  if (item > node->data)
-  {
-    node_t *new_node = malloc(sizeof(node_t));
-    new_node->data = item;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    node->right = new_node;
-  }
+  node->data = item;
 }
 
 void free_tree(node_t *tree)
